3c5b818e551efafc57e53d06acfdf28cf69727c0
MmeiyuC <>
2021-05-10 15:48:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let userInput
+
+// Connect "Submit" button to function
+document.getElementById('submit').addEventListener('click', age)
+
+function age () {
+  // Get user input and convert into an integer
+  userInput = document.getElementById('age').value
+  userInput = parseInt(userInput)
+
+  // Check users' age
+  if (userInput >= 17) {
+    document.getElementById('answer').innerHTML = 'You can see an R movie alone';
+  } else if (userInput >= 13) {
+    document.getElementById('answer').innerHTML = 'You can see an PG-13 movie alone';
+  } else if (userInput >= 5) {
+    document.getElementById('answer').innerHTML = 'You can see an G or PG moivie alone';
+  } else {
+    document.getElementById('answer').innerHTML = 'Uh. You\'re too young for most things';
+  }
+}
