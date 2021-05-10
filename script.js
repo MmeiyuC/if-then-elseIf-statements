15d5bd2a549305c1555d60e68e79b9b0fd56278f
MmeiyuC <>
2021-05-10 15:56:29 +0000

    edited

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function age () {
 
   // Check users' age
   if (userInput >= 17) {
-    document.getElementById('answer').innerHTML = 'You can see an R movie alone';
+    document.getElementById('answer').innerHTML = 'You can see an R movie alone'
   } else if (userInput >= 13) {
-    document.getElementById('answer').innerHTML = 'You can see an PG-13 movie alone';
+    document.getElementById('answer').innerHTML = 'You can see an PG-13 movie alone'
   } else if (userInput >= 5) {
-    document.getElementById('answer').innerHTML = 'You can see an G or PG moivie alone';
+    document.getElementById('answer').innerHTML = 'You can see an G or PG moivie alone'
   } else {
-    document.getElementById('answer').innerHTML = 'Uh. You\'re too young for most things';
+    document.getElementById('answer').innerHTML = 'Uh. You\'re too young for most things'
   }
 }
